71b3ad9767dea5bf2c9a370906e2b436aa633a70
karimkenn <karimkenn86@gmail.com>
2024-12-21 18:39:01 +0100

    Add files via upload

---
+++ operations.js
@@ -0,0 +1,16 @@
+let x = 75;
+let y = 25;
+let sum = x + y;
+console.log("addition:" +sum);
+let sub = x - y;
+console.log("subtraction:" +sub);
+let mul = x * y;
+console.log("multiplication:" +mul);
+let mod = x % y;
+console.log("modulus:" +mod);
+let exp = x ** y;
+console.log("expo:" +exp);
+
+
+
+
